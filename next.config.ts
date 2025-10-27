24a1699eaf6ddae7b4848d02213944a5dccdcbd6
anildagia <148940550+anildagia@users.noreply.github.com>
2025-10-27 17:25:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+    ...(module.exports?.experimental || {}),
+    serverComponentsExternalPackages: [
+      ...((module.exports?.experimental?.serverComponentsExternalPackages) || []),
+      '@sparticuz/chromium',
+      'puppeteer-core',
+    ],
+  },
 };
 
 export default nextConfig;
